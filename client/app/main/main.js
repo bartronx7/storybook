cc6d9d5582ded0faaece7dfa73b2df8358d527e3
Bart Akeley <bart.akeley@gmail.com>
2014-10-13 20:40:24 -0500

    initial generator output

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('storybookApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/storybook-dev'
+  },
+
+  seedDB: true
+};
